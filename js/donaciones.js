02a155cb59bd8db445986e4ba56c924966e05bd8
mathiax63 <mathyoyo@hotmail.es>
2021-10-28 15:47:45 -0300

    subo mejoras

--- js/donaciones.js
+++ js/donaciones.js
@@ -6,9 +6,12 @@ if(deseaDonar){
     let noDono = document.getElementById("noDono")
 let cantidad = prompt("Cuanto desea donar")
 let cantidadNumerica = parseInt(cantidad)
+let gracias = alert("Gracias por su donacion")
+let aviso = alert("En las proximas semanas recivira Emails del avane logrado gracias a su donacion")
 
 const cantidadActual = donacionesActuales  + cantidadNumerica;
 let cantidadDeDonaciones= document.getElementById("cantidadDentroDeDiv").innerHTML ="Gracias por su donacion actualmente las donaciones totales son de "+cantidadActual +"$ sumando la suya, GlobLife agradece de su donacion que ayudara a la adquicicion de materiales y expertos que ayudaran a limpiar los oceanos, mares y ayudara a la reforestacion y reconstrucion de oceanos,mares y bosques"
+
 
 siDona.style.display = "block";
 noDono.style.display = "none";
