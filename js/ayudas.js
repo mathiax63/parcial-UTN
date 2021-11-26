97ff4193228261583ffbadb79a1533fd272f4853
mathiax63 <mathyoyo@hotmail.es>
2021-11-26 17:03:47 -0300

    subo parte del segundo parcial

--- js/ayudas.js
+++ js/ayudas.js
@@ -30,18 +30,104 @@ if(mar && bosques){
 }
 
 
-let apellido = document.getElementById("apellido")
-
-let email = document.getElementById("email")
-
-
 let nombre1 = document.getElementById("nombre")
-let ubicacion = document.getElementById("lugar").value
-  
-let validador = new RegExp(ubicacion[lugar])
+let nombre1Mar = document.getElementById("nombreMar")
+let apellido = document.getElementById("apellido")
+let apellidoMar = document.getElementById("apellidoMar")
+ let email = document.getElementById("email") 
+ let emailMar = document.getElementById("emailMar") 
+ 
 
-if(validador.test(codigo.value)){
-    nombre1.setCustomValidity("")
-}else{
-    nombre1.setCustomValidity()
+
+
+function validarCredenciales(){
+    nombre1.addEventListener("input", validarAlUsuario)
+    apellido.addEventListener("input", validarApellido)
+    email.addEventListener("input", validarSuEmail)
+    nombre1Mar.addEventListener("input", validarAlUsuarioMar)
+    apellidoMar.addEventListener("input", validarApellidoMar)
+    emailMar.addEventListener("input", validarSuEmailMar)
+
+
+
+
+    validarAlUsuario()
+    validarApellido()
+    validarSuEmail()
+    validarAlUsuarioMar()
+    validarApellidoMar()
+    validarSuEmailMar()
 }
+
+function validarAlUsuario(){
+    if(nombre1.value == "") {
+        nombre1.style.backgroundColor = "#ff8080"
+        nombre1.setCustomValidity("Ingrese su nombre")
+
+    }else{
+        nombre1.style.backgroundColor = "#adebad"
+        nombre1.setCustomValidity("")
+    }
+
+}
+
+function validarSuEmail(){
+    if(email.value == "") {
+        email.style.backgroundColor = "#ff8080"
+        email.setCustomValidity("Ingrese su email para poder contactarlo")
+
+    }else{
+        email.style.backgroundColor = "#adebad"
+        email.setCustomValidity("")
+    }
+
+}
+function validarApellido(){
+if(apellido.value == ""){
+    apellido.style.backgroundColor = "#ff8080"
+    apellido.setCustomValidity("Ingrese su apellido")
+}else{
+    apellido.style.backgroundColor = "#adebad"
+    apellido.setCustomValidity("")
+}
+
+}
+function validarAlUsuarioMar(){
+    if(nombre1Mar.value == "") {
+        nombre1Mar.style.backgroundColor = "#ff8080"
+        nombre1Mar.setCustomValidity("Ingrese su nombre")
+
+    }else{
+        nombre1Mar.style.backgroundColor = "#adebad"
+        nombre1Mar.setCustomValidity("")
+    }
+
+}
+function validarApellidoMar(){
+    if(apellidoMar.value == ""){
+        apellidoMar.style.backgroundColor = "#ff8080"
+        apellidoMar.setCustomValidity("Ingrese su apellido")
+    }else{
+        apellidoMar.style.backgroundColor = "#adebad"
+        apellidoMar.setCustomValidity("")
+    }
+    
+    }
+function validarSuEmailMar(){
+    if(emailMar.value == "") {
+        emailMar.style.backgroundColor = "#ff8080"
+        emailMar.setCustomValidity("Ingrese su email para poder contactarlo")
+
+    }else{
+        emailMar.style.backgroundColor = "#adebad"
+        emailMar.setCustomValidity("")
+    }
+
+}
+
+
+
+
+
+
+window.addEventListener("load", validarCredenciales)
